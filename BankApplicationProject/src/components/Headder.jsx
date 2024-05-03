5b9f652c709acb8ffb87bfe0c11e1d1928daa0ed
talatamSubhash <159922473+talatamSubhash@users.noreply.github.com>
2024-05-03 22:10:20 +0530

    Update Headder.jsx

--- BankApplicationProject/src/components/Headder.jsx
+++ BankApplicationProject/src/components/Headder.jsx
@@ -8,15 +8,15 @@ const Headder = () => {
             <h1 style={{textAlign:"center"}}>SDFC BANK</h1>
             <h3 style={{textAlign:"center"}}>EXTRA ORDINARY SERVICES</h3>
         </div>
-        <div>
-        <Link to = '/' style={{marginRight:100}}>Home</Link>
-        <Link to = '/newAccount' style={{marginRight:100}}>New Account</Link>
-        <Link to = '/viewBalance' style={{marginRight:100}}>Balance</Link>
-        <Link to = '/addBalance' style={{marginRight:100}}>Deposit</Link>
-        <Link to = '/withdrawBalance' style={{marginRight:100}}>Withdraw</Link>
-        <Link to = '/transferBalance' style={{marginRight:100}}>Transfer</Link>
-        <Link to = '/closeAccount' style={{marginRight:100}}>Close Account</Link>
-        <Link to = '/about' style={{marginRight:100}}>AboutUs</Link>
+        <div style={{backgroundImage:`URL("https://www.cossacklabs.com/case-studies/african-bank/preview_hub90103ed64272ab4cc07310463ff3dff_85916_768x0_resize_q75_box.jpg")`,backgroundSize:"200px",height:"200px",paddingTop:"20px",paddingLeft:"90px"}}>
+        <Link to = '/' style={{marginRight:100}}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>Home</button></Link>
+        <Link to = '/newAccount' style={{marginRight:100,color:"white"}}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>New Account</button></Link>
+        <Link to = '/viewBalance' style={{marginRight:100,color:"white"}}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>Balance</button></Link>
+        <Link to = '/addBalance' style={{marginRight:100,color:"white"}}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>Deposit</button></Link>
+        <Link to = '/withdrawBalance' style={{marginRight:100,color:"white"}}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>Withdraw</button></Link>
+        <Link to = '/transferBalance' style={{marginRight:100},color:"white"}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>Transfer</button></Link>
+        <Link to = '/closeAccount' style={{marginRight:100,color:"white"}}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>Close Account</button></Link>
+        <Link to = '/about' style={{marginRight:100,color:"white"}}><button style={{background:"blue",color:"white",padding:"3px",borderRadius:"10px",fontWeight:"bold",cursor:"pointer"}}>AboutUs</button></Link>
         </div>
     </>);
 }
