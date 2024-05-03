6a0aa948edd416a44b9826db986cb40c6a834e35
talatamSubhash <159922473+talatamSubhash@users.noreply.github.com>
2024-05-03 21:47:52 +0530

    Update About.jsx

--- BankApplicationProject/src/components/About.jsx
+++ BankApplicationProject/src/components/About.jsx
@@ -2,6 +2,11 @@ import React from "react";
 const About =()=>{
     return(<>
         <div><h1 style={{textAlign:"center"}}>Welcome To About Page</h1></div>
+         <h1 style={{border:"2px solid black",textAlign:"center",color:"blue",background:"white"}}>SDFC basic online bank</h1></div>
+        <h2 style={{textAlign:"center"}}>main branch: Hyderabad,SR nagar,near pillar no:A 1012</h2>
+        <h2 style={{textAlign:"center"}}>contact has:9618983251</h2>
+
+        <img style={{width:'700px',height: '250px' }} src="https://th.bing.com/th/id/OIP.1FlEt2rufp_ZB7kRJ8Rc2wAAAA?rs=1&pid=ImgDetMain"/> 
         </>)
 }
 export default About;
