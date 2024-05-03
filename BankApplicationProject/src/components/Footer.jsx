a939613f4bd7b1a7c53217c72cc1aeaaa8278b86
talatamSubhash <159922473+talatamSubhash@users.noreply.github.com>
2024-05-03 22:05:15 +0530

    Update Footer.jsx

--- BankApplicationProject/src/components/Footer.jsx
+++ BankApplicationProject/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Footer = ()=>{
     return(<>
-        <h2>@COPYRIGHTS-SATHYATECH-2024</h2>
+      <h2 style={{backgroundImage:`URL("https://www.shutterstock.com/image-illustration/binary-matrix-code-numbers-computer-260nw-1947731290.jpg")`,color:"white",height:"200px",backgroundSize:"80%",textAlign:"center"}}>@COPYRIGHTS-Talatam subhash</h2>
         </>)
 }
 export default Footer;
