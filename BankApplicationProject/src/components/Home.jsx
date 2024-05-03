a0978c29d57b50efdc3cdef342cf228bce448e07
talatamSubhash <159922473+talatamSubhash@users.noreply.github.com>
2024-05-03 22:11:17 +0530

    Update Home.jsx

--- BankApplicationProject/src/components/Home.jsx
+++ BankApplicationProject/src/components/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 const Home =()=>{
     return(<>
         <div ><h1 style={{textAlign:"center"}}>Welcome To Home Page</h1></div>
+         <h2 style={{textAlign:"center",border:"2px solid black",color:"blue"}}>Easy to send money and deposit money securely</h2></div>
+        <img style={{width:'700px',height:'300px'}}src="https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/UD7CEz6/bank-bank-building-branch_vyks9ofi_thumbnail-1080_01.png"/>
         </>)
 }
 export default Home;
